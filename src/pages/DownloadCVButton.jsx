7a4add7bec78e1d6a47fc4801f3abfcba4988092
Muasa-harman <harman.muasa@gmail.com>
2024-01-17 23:38:54 +0300

    deleted download button

--- src/pages/DownloadCVButton.jsx
+++ src/pages/DownloadCVButton.jsx
@@ -1,26 +1,9 @@
 import React from 'react';
 
 const DownloadCVButton = () => {
-  const handleDownload = () => {
-    // Create CV content
-    const cvContent = {}
-
-    // Create a Blob from the content
-    const blob = new Blob([cvContent], { type: 'text/plain' });
-
-    // Create a download link
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = 'Your_CV.txt';
-
-    // Append the link to the body, trigger the click, and remove the link
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
 
   return (
-    <button onClick={handleDownload}>
+    <button>
       Download CV
     </button>
   );
